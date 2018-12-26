cef4958fabc67d13cf104b4da848bb624934d28b
lamborghini0922 <46084737+lamborghini0922@users.noreply.github.com>
2018-12-27 01:52:05 +0900

    refactoring
    refactoring

--- src/Auth.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-
-const Auth = props =>
-  props.status === 1 ? props.children : <Redirect to={"/login"} />;
-
-export default Auth;
